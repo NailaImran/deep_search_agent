11ab70b4e9659598781ecfb067a273125fd68a99
Naila Imran <143532950+NailaImran@users.noreply.github.com>
2025-08-23 16:37:13 +0500

    Update gemini_config.py

--- gemini_config.py
+++ gemini_config.py
@@ -2,17 +2,11 @@ import os
 from dotenv import load_dotenv
 from agents import Agent, AsyncOpenAI, ModelSettings, OpenAIChatCompletionsModel
 import requests
-#from tavily import AsyncTavilyClient, TavilyClient
-
-
-
 
 load_dotenv()
 gemini_api_key = os.getenv("GEMINI_API_KEY")
 openai_api_key = os.getenv("OPENAI_API_KEY")
-#TAVILY_API_KEY = os.getenv("TAVILY_API_KEY")
 
-#tavily_client = TavilyClient(api_key=TAVILY_API_KEY)
 # 1. Which LLM Service?
 external_client: AsyncOpenAI = AsyncOpenAI(
     api_key=gemini_api_key,
@@ -24,3 +18,4 @@ llm_model: OpenAIChatCompletionsModel = OpenAIChatCompletionsModel(
     model="gemini-2.5-flash",
     openai_client=external_client
 )
+
